f4f8613db398184b554cd0fe0bf212322d6e903c
Marcela Ang <marcela.ang@gmail.com>
2022-10-20 11:02:26 -0400

    M1W1 - Implementing Tail

---
+++ tail.js
@@ -0,0 +1,26 @@
+//given array, extract the head and create a new array with the remainder
+
+let tail = function(arg) {
+  let arrTail = arg.slice(1);
+  return arrTail;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+
+// Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
