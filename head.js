4e9debed5293d8fd5ffcf5a9e6ce9be058fc1970
Marcela Ang <marcela.ang@gmail.com>
2022-10-20 10:04:24 -0400

    Eslint done

--- head.js
+++ head.js
@@ -1,17 +1,17 @@
 let head = function(arg) {
-    let arrHead = arg[0];
-    return arrHead;
-  };
+  let arrHead = arg[0];
+  return arrHead;
+};
 
-  const assertEqual = function(actual, expected) {
-    if (actual !== expected) {
-      console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
-    } else {
-      console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
-    }
-  };
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
+  }
+};
 
-  assertEqual(head([9,6,7]), 5);
-  assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([9,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
 
