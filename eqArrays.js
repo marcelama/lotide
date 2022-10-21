03699e49c598b9a24dd1f5694dd8632bbc64e51b
Marcela Ang <marcela.ang@gmail.com>
2022-10-21 17:24:46 -0400

    added condition if array lengths are different to return false

--- eqArrays.js
+++ eqArrays.js
@@ -1,9 +1,11 @@
 //Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match:
 
-
-const eqArrays = (array1, array2) => {
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
+const eqArrays = (arg1, arg2) => {
+  if (arg1.length !== arg2.length) {
+    return false;
+  }
+  for (let i = 0; i < arg1.length; i++) {
+    if (arg1[i] !== arg2[i]) {
       return false;
     }
   }
