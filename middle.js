3b2a753d47891c886c7d54284d32bb2bd2a985d3
Marcela Ang <marcela.ang@gmail.com>
2022-10-21 17:22:28 -0400

    M1W1 - Middle

---
+++ middle.js
@@ -0,0 +1,42 @@
+const eqArrays = (arg1, arg2) => {
+  if (arg1.length !== arg2.length) {
+    return false;
+  }
+  for (let i = 0; i < arg1.length; i++) {
+    if (arg1[i] !== arg2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (!eqArrays(actual, expected)) {
+    console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+//Implement middle which will take in an array and return the middle-most element(s) of the given array.
+const middle = function(array) {
+  let arrLen = array.length;
+  let middle = 0;
+  let middleResult = [];
+  if (arrLen === 1 || arrLen === 2) { // if === 1 || ===2 return []
+    return [];
+  } else if (arrLen > 2 && arrLen % 2 === 0) {
+    middle = (arrLen / 2) - 1;
+    middleResult = [array[middle], array[middle + 1]];
+    return middleResult; // if > 2 && % 2 === 0 (even) => the 2 numbers in the middle
+  } else if (arrLen > 2 && arrLen !== 0) {
+    middle = (arrLen / 2) - 0.5;
+    middleResult = [array[middle]];
+    return middleResult; // if > 2 && %2 !== 0 (odd) => one number in the middle
+  }
+};
+
+//TEST CODE
+console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]));
+console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]));
+console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]));
