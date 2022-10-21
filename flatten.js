a341ca9e789f0560cda0febeff4a915efad792b5
Marcela Ang <marcela.ang@gmail.com>
2022-10-21 15:12:18 -0400

    M1W1 - Flatten

---
+++ flatten.js
@@ -0,0 +1,11 @@
+//Create a function flatten which will take in an array containing elements including nested arrays of elements, and return a "flattened" version of the array.
+
+const flatten = (array) => {
+  if (Array.isArray(array)){
+  let flatArray = [].concat(...array);
+  console.log(flatArray);
+  }
+}
+
+// Test
+flatten([1, 2, [3, 4], 5, [6]])
