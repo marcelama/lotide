212620984af9365cb3c3e7eed69cab7d0a8c0365
Marcela Ang <marcela.ang@gmail.com>
2022-10-21 11:04:08 -0400

    Ran eslint

--- eqArrays.js
+++ eqArrays.js
@@ -2,13 +2,13 @@
 
 
 const eqArrays = (array1, array2) => {
-  for (let i = 0; i < array1.length; i++){
+  for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
     }
   }
   return true;
-}
+};
 
 const assertEqual = function(array1, array2) {
   if (array1 !== array2) {
