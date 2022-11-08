59de340902e65e459cab4c39807730758e34aeff
Marcela Ang <marcela.ang@gmail.com>
2022-11-08 17:58:00 -0500

    M1W4 - refactor

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,17 @@
-const eqArrays = (arg1, arg2) => {
-  if (arg1.length !== arg2.length) {
-    return false;
-  }
-  for (let i = 0; i < arg1.length; i++) {
-    if (arg1[i] !== arg2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
+
+// const eqArrays = (arg1, arg2) => {
+//   if (arg1.length !== arg2.length) {
+//     return false;
+//   }
+//   for (let i = 0; i < arg1.length; i++) {
+//     if (arg1[i] !== arg2[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+
 
 const assertArraysEqual = function(actual, expected) {
   if (!eqArrays(actual, expected)) {
@@ -18,7 +21,8 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
+module.exports = assertArraysEqual;
 
-console.log(assertArraysEqual([1, 2, 3, 4], [1, 3, 4, 5]));
-console.log(assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 4]));
+// console.log(assertArraysEqual([1, 2, 3, 4], [1, 3, 4, 5]));
+// console.log(assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 4]));
 
