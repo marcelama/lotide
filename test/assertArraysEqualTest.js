60c3d53a942ea0ee0f2a385ff3b2b2fd2b4597ec
Marcela Ang <marcela.ang@gmail.com>
2022-11-08 17:57:22 -0500

    M1W4 - refactor

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,5 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+
+console.log(assertArraysEqual([1, 2, 3, 4], [1, 3, 4, 5]));
+console.log(assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 4]));
