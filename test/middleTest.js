50c0b1c13f4c5cfee64446962a6e6bfd453e405a
Marcela Ang <marcela.ang@gmail.com>
2022-11-09 11:40:30 -0500

    M1W4 - Mocha and Chai incorporated

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,18 @@
+const { assert } = require('chai');
 const middle = require('../middle');
-const  assertArraysEqual = require('../assertArraysEqual');
+// const  assertArraysEqual = require('../assertArraysEqual'); //no longer needed as we're using Chai (assertion library)
 
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]));
+describe("#middle", () => {
+
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it("returns [4] for [1, 2, 3, 4, 5, 6, 7]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
+  });
+});
