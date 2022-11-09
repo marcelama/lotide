7e10a1541404ac8bd61fb38da92c87916e70fdff
Marcela Ang <marcela.ang@gmail.com>
2022-11-09 11:31:59 -0500

    M1W4 - Mocha and Chai incorporated

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,15 +1,15 @@
-const assertEqual = require('../assertEqual');
+const { assert } = require('chai');
+// const assertEqual = require('../assertEqual'); //no longer needed as we're using Chai (assertion library)
 const tail = require('../tail');
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+//assert.deepEqual can compare arrays and objects
+describe("#tail", () => {
 
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']),['Lighthouse', 'Labs']);
+  });
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+});
