bf286017bca9faf93495c3aa9ed0a2af4741d296
Marcela Ang <marcela.ang@gmail.com>
2022-11-08 19:54:57 -0500

    M1W4 - refactor

---
+++ test/middleTest.js
@@ -0,0 +1,6 @@
+const middle = require('../middle');
+const  assertArraysEqual = require('../assertArraysEqual');
+
+console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]));
+console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]));
+console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]));
