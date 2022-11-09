5377c78c99a52f9726e832eab6ca9918fb8b3635
Marcela Ang <marcela.ang@gmail.com>
2022-11-09 11:07:37 -0500

    M1W4 - Chai and Mocha incorporated

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,22 @@
-const assertEqual = require('../assertEqual');
+const { assert } = require('chai');
+// const assertEqual = require('../assertEqual'); no longer needed as we're using Chai (assertion library)
 const head = require('../head');
 
-assertEqual(head([9,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns 9 for [9,6,7]", () => {
+    assert.strictEqual(head([9,6,7]), 9);
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), 'Hello');
+  });
+});
